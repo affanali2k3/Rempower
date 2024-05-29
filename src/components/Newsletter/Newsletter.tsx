1d2998133cb044ecf93ebf584a3e8cd4cddd560e
affanali2k3 <affanalikhanxx@gmail.com>
2024-05-29 22:16:46 +0500

    [NEWSLETTER FORM EMBEDDING CHANGED]

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -6,31 +6,32 @@ export const Newsletter = () => {
   const formRef = useRef<HTMLFormElement>(null);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    // event.preventDefault();
-    // Handle form submission here
-    // if (formRef.current) {
-    //   const formData = new FormData(formRef.current);
-    //   const email = formData.get("email") as string;
-    //   if (!validateEmail(email)) {
-    //     alert("Please enter a valid email address.");
-    //     return;
-    //   } // Example: send the form data to an API
-    //   fetch(formRef.current.action, {
-    //     method: "POST",
-    //     body: formData,
-    //   })
-    //     .then((response) => {
-    //       console.log(response);
-    //       if (response.ok) {
-    //         console.log("Form submitted successfully");
-    //       } else {
-    //         console.error("Form submission error");
-    //       }
-    //     })
-    //     .catch((error) => {
-    //       console.error("Form submission error", error);
-    //     });
-    // }
+    event.preventDefault();
+
+    if (formRef.current) {
+      const formData = new FormData(formRef.current);
+      const email = formData.get("email") as string;
+      if (!validateEmail(email)) {
+        alert("Please enter a valid email address.");
+        return;
+      } // Example: send the form data to an API
+      fetch(formRef.current.action, {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          console.log(response);
+          if (response.ok) {
+            console.log("Form submitted successfully");
+          } else {
+            console.error("Form submission error");
+          }
+        })
+        .catch((error) => {
+          console.error("Form submission error", error);
+        });
+    }
+    window.location.href = "thank-you";
   };
 
   const validateEmail = (email: string) => {
@@ -47,8 +48,8 @@ export const Newsletter = () => {
           questions, you can enter your details and will get back to you as soon as possible.
         </p>
         <div className="inputs">
-          <form ref={formRef} method="post" action="https://systeme.io/embedded/18168894/subscription" onSubmit={handleSubmit}>
-            <input required={true} type="text" name="surname" placeholder="First Name" />
+          <form ref={formRef} method="post" action="https://systeme.io/embedded/18252271/subscription" onSubmit={handleSubmit}>
+            <input required={true} type="text" name="first_name" placeholder="First Name" />
             <input required={true} type="text" name="email" placeholder="Email" />
             <button type="submit" className="join-button btn">
               JOIN THE COMMUNITY
